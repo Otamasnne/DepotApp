4c10de222666ad72471b87a7c9e89e2f1057ff95
santi <santi.imas37@gmail.com>
2022-06-13 04:57:11 -0300

    ISSUE #2: prueba

--- module-simple/src/main/java/domainapp/modules/simple/dom/proveedor/Proveedor.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/proveedor/Proveedor.java
@@ -2,13 +2,15 @@ package domainapp.modules.simple.dom.proveedor;
 
 import domainapp.modules.simple.dom.articulo.Articulo;
 
+import javax.jdo.annotations.PersistenceCapable;
+
+@PersistenceCapable
 public class Proveedor implements Comparable<Proveedor>{
 
 
 
-
     @Override
-    public int compareTo(Articulo articulo) {
+    public int compareTo(Proveedor proveedor) {
         return 0;
     }
 }
--- webapp/src/main/java/domainapp/webapp/application/services/homepage/HomePageViewModel.java
+++ webapp/src/main/java/domainapp/webapp/application/services/homepage/HomePageViewModel.java
@@ -4,12 +4,17 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import domainapp.modules.simple.config.Config;
 import domainapp.modules.simple.dom.articulo.Articulo;
 import domainapp.modules.simple.dom.articulo.Articulos;
+import domainapp.modules.simple.service.IService;
+import domainapp.modules.simple.service.Service;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.HomePage;
 import org.apache.isis.applib.annotation.Nature;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 @DomainObject(
@@ -19,6 +24,7 @@ import org.apache.isis.applib.annotation.Nature;
 @HomePage
 @DomainObjectLayout()
 public class HomePageViewModel {
+
 
     public String title() {
         return getObjects().size() + " artículos";
