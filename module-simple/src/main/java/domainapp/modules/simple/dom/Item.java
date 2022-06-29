c84f092669b0777c6901ee36fce95fdf8fb0618f
MartinLnn <martin.lacabanne@gmail.com>
2022-06-29 20:06:56 -0300

    Issue #5: Redundancias eliminadas en Item, se suma el metodo constructor estático.

--- module-simple/src/main/java/domainapp/modules/simple/dom/Item.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/Item.java
@@ -1,22 +1,16 @@
 package domainapp.modules.simple.dom;
 
 import domainapp.modules.simple.dom.articulo.Articulo;
-import domainapp.modules.simple.types.articulo.Codigo;
 import domainapp.modules.simple.types.comprobante.CantidadMueve;
 import lombok.*;
 import org.apache.isis.applib.annotation.DomainObject;
 import org.apache.isis.applib.annotation.DomainObjectLayout;
 import org.apache.isis.applib.annotation.Publishing;
 import org.apache.isis.applib.jaxb.PersistentEntityAdapter;
-import org.apache.isis.applib.services.message.MessageService;
-import org.apache.isis.applib.services.repository.RepositoryService;
-import org.apache.isis.applib.services.title.TitleService;
 
-import javax.inject.Inject;
 import javax.jdo.annotations.IdGeneratorStrategy;
 import javax.jdo.annotations.VersionStrategy;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
-import java.util.List;
 
 @javax.jdo.annotations.DatastoreIdentity(strategy= IdGeneratorStrategy.IDENTITY, column="id")
 @javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column="version")
@@ -27,13 +21,12 @@ import java.util.List;
 @ToString(onlyExplicitlyIncluded = true)
 public class Item {
 
-    @Inject
-    RepositoryService repositoryService;
-    @Inject
-    TitleService titleService;
-    @Inject
-    MessageService messageService;
-
+    public static Item creacionItem(Articulo articulo, int cantidad) {
+        val item = new Item();
+        item.setArticulo(articulo);
+        item.setCantidad(cantidad);
+        return item;
+    }
 
     @Getter
     @Setter
