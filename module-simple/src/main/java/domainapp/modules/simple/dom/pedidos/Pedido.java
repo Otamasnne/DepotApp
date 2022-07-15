d0f1d94520e86b8a92870bc4c01523d10748b1aa
santi <santi.imas37@gmail.com>
2022-07-15 17:27:02 -0300

    ISSUE #6: se creo clase pedido

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/pedidos/Pedido.java
@@ -0,0 +1,58 @@
+package domainapp.modules.simple.dom.pedidos;
+
+import domainapp.modules.simple.dom.articulo.Articulo;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.jaxb.PersistentEntityAdapter;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.IdentityType;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.VersionStrategy;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Comparator;
+
+@PersistenceCapable(
+        schema = "depotapp",
+        identityType= IdentityType.DATASTORE
+)
+@javax.jdo.annotations.Unique(
+        name = "Articulo_codigo_UNQ", members = {"codigo"}
+)
+@javax.jdo.annotations.DatastoreIdentity(strategy= IdGeneratorStrategy.IDENTITY, column="id")
+@javax.jdo.annotations.Version(strategy= VersionStrategy.DATE_TIME, column="version")
+@DomainObject(logicalTypeName = "depotapp.Pedido", entityChangePublishing = Publishing.ENABLED)
+@DomainObjectLayout()
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@XmlJavaTypeAdapter(PersistentEntityAdapter.class)
+@ToString(onlyExplicitlyIncluded = true)
+@javax.persistence.Table(schema = "SIMPLE")
+public class Pedido implements Comparable<Pedido> {
+
+
+    @Inject
+    RepositoryService repositoryService;
+    @Inject
+    TitleService titleService;
+    @Inject
+    MessageService messageService;
+
+    @
+    private String codigo;
+
+
+    private final static Comparator<Pedido> comparator =
+            Comparator.comparing(Pedido::getCodigo);
+    @Override
+    public int compareTo(final Pedido other) {
+        return comparator.compare(this, other);
+    }
+}
