67c86369129ada5ae7d983f8a376c3068cbc3a72
santi <santi.imas37@gmail.com>
2022-07-26 19:14:53 -0300

    ISSUE #6: pruebas con pedido

--- module-simple/src/main/java/domainapp/modules/simple/dom/articulo/ArticuloRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/articulo/ArticuloRepository.java
@@ -11,41 +11,46 @@ import javax.jdo.Transaction;
 import java.util.List;
 
 @Repository
-public class ArticuloRepository implements IAritculoRepository {
-
-    @Inject
-    private PersistenceManagerFactory pmf;
-    @Override
-    public List<Articulo> findByPedido(Pedido pedido) {
-        Articulo articulo = null;
-
-        PersistenceManager pm = this.pmf.getPersistenceManager();
-        Transaction tx = pm.currentTransaction();
-
-        try {
-
-            tx.begin();
-
-            JDOQLTypedQuery<Articulo> tq = pm.newJDOQLTypedQuery(Articulo.class);
+public interface ArticuloRepository {
 
 
+    List<Articulo> findByArt(Articulo articulo);
 
-//            QArticulo cand = QArticulo.candidate();
-//            QPedido var = QPedido.variable("var");
 
-//            articulo = tq.filter(cand.numero.eq(numero)).executeUnique();
+//    @Inject
+//    private PersistenceManagerFactory pmf;
+//    @Override
+//    public List<Articulo> findByPedido(Pedido pedido) {
+//        Articulo articulo = null;
+//
+//        PersistenceManager pm = this.pmf.getPersistenceManager();
+//        Transaction tx = pm.currentTransaction();
+//
+//        try {
+//
+//            tx.begin();
+//
+//            JDOQLTypedQuery<Articulo> tq = pm.newJDOQLTypedQuery(Articulo.class);
+//
+//
+//
+////            QArticulo cand = QArticulo.candidate();
+////            QPedido var = QPedido.variable("var");
+//
+////            articulo = tq.filter(cand.numero.eq(numero)).executeUnique();
+//
+//            tx.commit();
+//        }
+//        finally {
+//            if(tx.isActive()) {
+//                tx.rollback();
+//            }
+//            pm.close();
+//        }
+//
+//        return (List<Articulo>) articulo;
+//    }
 
-            tx.commit();
-        }
-        finally {
-            if(tx.isActive()) {
-                tx.rollback();
-            }
-            pm.close();
-        }
 
-        return (List<Articulo>) articulo;
-    }
-
-    }
+}
 
