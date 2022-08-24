e0042144b4bce88e7d47087844952857518d05ee
santi <santi.imas37@gmail.com>
2022-08-24 10:36:38 -0300

    ISSUE #6: se creo paquete para itemPedido y clase ItemPedido
    Aca van a ir todas las clases relacionadas a los items para armar los pedidos
    Cree la clase ItemPedido y su implementacion.

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/itemPedido/ItemPedido.java
@@ -0,0 +1,89 @@
+package domainapp.modules.simple.dom.itemPedido;
+
+
+import domainapp.modules.simple.dom.articulo.Articulo;
+import domainapp.modules.simple.dom.pedidos.Pedido;
+import domainapp.modules.simple.types.comprobante.CantidadMueve;
+import lombok.*;
+import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.DomainObjectLayout;
+import org.apache.isis.applib.annotation.PropertyLayout;
+import org.apache.isis.applib.annotation.Publishing;
+import org.apache.isis.applib.jaxb.PersistentEntitiesAdapter;
+import org.apache.isis.applib.services.message.MessageService;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.services.title.TitleService;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import javax.jdo.annotations.*;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.util.Comparator;
+
+@PersistenceCapable(
+        schema = "depotapp",
+        identityType = IdentityType.DATASTORE
+)
+@Unique(
+        name = "ItemPedido_articulo_pedido_UNQQ", members = {"articulo","kitArticulo"}
+)
+@Queries(
+        {
+                @Query(
+                        name = ItemPedido.NAMED_QUERY_BUSCAR_ITEM_POR_PEDIDO,
+                        value = "SELECT " +
+                                "FROM domainapp.modules.simple.dom.item.ItemPedido " +
+                                "WHERE pedido.indexOf(:Pedido) >= 0 "
+                )
+        }
+)
+@DatastoreIdentity(strategy = IdGeneratorStrategy.IDENTITY, column = "id")
+@Version(strategy = VersionStrategy.DATE_TIME, column = "version")
+@DomainObject(logicalTypeName = "depotapp.ItemPedido", entityChangePublishing = Publishing.ENABLED)
+@DomainObjectLayout()
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@XmlJavaTypeAdapter(PersistentEntitiesAdapter.class)
+@ToString(onlyExplicitlyIncluded = true)
+@Table(schema = "SIMPLE")
+public class ItemPedido implements Comparable<ItemPedido> {
+
+    @Inject
+    RepositoryService repositoryService;
+    @Inject
+    TitleService titleService;
+    @Inject
+    MessageService messageService;
+
+    static final String NAMED_QUERY_BUSCAR_ITEM_POR_PEDIDO = "ItemPedido.buscarItemPorPedido";
+
+    ItemPedido(Pedido pedido, Articulo articulo, int cantidad){
+        val item = new ItemPedido();
+
+    }
+
+    @Getter@Setter@ToString.Include
+    @Column(allowsNull = "false") //REVISAR ESTO
+    @PropertyLayout(fieldSetId = "itemPedido", sequence = "1")
+    private Articulo articulo;
+
+    @Getter@Setter@ToString.Include
+    @CantidadMueve
+    @PropertyLayout(fieldSetId = "itemPedido", sequence = "2")
+    private int cantidad;
+
+    @Getter@Setter@ToString.Include
+    @Column(allowsNull = "false")
+    @PropertyLayout(fieldSetId = "ItemPedido", sequence = "3")
+    private Pedido pedido;
+
+
+
+    private final static Comparator<ItemPedido> comparator =
+            Comparator.comparing(ItemPedido::getPedido).thenComparing(ItemPedido::getArticulo);
+
+    @Override
+    public int compareTo(final ItemPedido other) {
+        return comparator.compare(this, other);
+    }
+}
--- module-simple/src/main/java/domainapp/modules/simple/dom/pedidos/Pedido.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/pedidos/Pedido.java
@@ -1,5 +1,6 @@
 package domainapp.modules.simple.dom.pedidos;
 
+import domainapp.modules.simple.dom.Item;
 import domainapp.modules.simple.dom.articulo.Articulo;
 import domainapp.modules.simple.dom.kitArticulo.KitArticulo;
 import domainapp.modules.simple.types.pedido.CodigoPedido;
@@ -19,6 +20,7 @@ import javax.jdo.annotations.VersionStrategy;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Vector;
 
 @PersistenceCapable(
         schema = "depotapp",
@@ -37,10 +39,10 @@ import java.util.List;
 @javax.persistence.Table(schema = "SIMPLE")
 @javax.jdo.annotations.Queries({
         @javax.jdo.annotations.Query(
-                name = Pedido.NAMED_QUERY__FIND_BY_CODIGO_EXACT,
+                name = Pedido.findByPedido,
                 value = "SELECT " +
                         "FROM domainapp.modules.simple.dom.articulo.Articulo " +
-                        "WHERE codigo == :codigo"
+                        "WHERE pedido == :pedido "
         )
 })
 public class Pedido implements Comparable<Pedido> {
@@ -53,17 +55,18 @@ public class Pedido implements Comparable<Pedido> {
     @Inject
     MessageService messageService;
 
-    static final String NAMED_QUERY__FIND_BY_CODIGO_EXACT = "Articulo.findByCodigoExact";
+    static final String findByPedido = "Articulo.findByCodigoExact";
 
     @Title
     @CodigoPedido
     @Getter@Setter @ToString.Include
     private String codigo;
 
-//    @Getter@Setter@ToString.Include
-//    @Collection
-//    @PropertyLayout(fieldSetId = "kitArticulo", sequence ="3" )
-//    private List<Articulo> articulos;
+    @Getter@Setter@ToString.Include
+    @Collection
+    @PropertyLayout(fieldSetId = "pedido", sequence ="3" )
+    private Vector<Item> items;
+
 
     public static Pedido withName(String codigo) {
         val pedido = new Pedido();
@@ -74,12 +77,70 @@ public class Pedido implements Comparable<Pedido> {
 
     public Articulo findByCodigoExact(final String codigo) {
         return repositoryService.firstMatch(
-                        Query.named(Articulo.class, Pedido.NAMED_QUERY__FIND_BY_CODIGO_EXACT)
+                        Query.named(Articulo.class, Pedido.findByPedido)
                                 .withParameter("codigo", codigo))
                 .orElse(null);
     }
 
 
+//    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+//    //@ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+//    @ActionLayout(associateWith = "articulos")
+//    public void addItem(Articulo newItem){
+//
+//        //comprobar si ya existe este item en el pedido
+//        int index = items.indexOf(newItem);
+//
+//        // fragmento de evento de pedido
+//        if (index == -1) {
+//            // Si el item es nuevo lo agrego
+//            items.addElement(newItem);
+//        }else {
+//            //si ya existe, agrego cantidad
+//            Articulo currItem = (Articulo) items.elementAt(index);
+//
+//            //currItem.add(newItem);
+//        }
+//
+//    }
+
+//    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+//    //@ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+//    @ActionLayout(associateWith = "articulos")
+//    public void addItem(Articulo newItem){
+//
+//        //comprobar si ya existe este item en el pedido
+//        int index = items.indexOf(newItem);
+//
+//        // fragmento de evento de pedido
+//        if (index == -1) {
+//            // Si el item es nuevo lo agrego
+//            items.addElement(newItem);
+//        }else {
+//            //si ya existe, agrego cantidad
+//            Articulo currItem = (Articulo) items.elementAt(index);
+//
+//            //currItem.add(newItem);
+//        }
+//
+//    }
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    //@ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    @ActionLayout(associateWith = "articulos")
+    public void addArticulo(Articulo item, int cantidad){
+        Item objetivo = Item.creacionItem(item, cantidad);
+
+        repositoryService.persist(items.add(objetivo));
+    }
+//    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+//    //@ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+//    @ActionLayout(associateWith = "articulos")
+//    public void addArticulo(String item){
+//        Item objetivo = findByCodigoExact(item);
+//
+//        repositoryService.persist(items.add(objetivo));;
+//    }
 
     private final static Comparator<Pedido> comparator =
             Comparator.comparing(Pedido::getCodigo);
