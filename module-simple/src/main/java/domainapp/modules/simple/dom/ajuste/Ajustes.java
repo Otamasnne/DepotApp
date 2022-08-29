e810e083a65668aa72e12450fcddb9fed732a1df
MartinLnn <martin.lacabanne@gmail.com>
2022-08-29 19:36:15 -0300

    Issue #9: Se crean las acciones para crear ajustes tanto positivo como negativo

---
+++ module-simple/src/main/java/domainapp/modules/simple/dom/ajuste/Ajustes.java
@@ -0,0 +1,69 @@
+package domainapp.modules.simple.dom.ajuste;
+
+import domainapp.modules.simple.dom.cliente.Cliente;
+import domainapp.modules.simple.types.comprobante.CodigoCo;
+import org.apache.isis.applib.annotation.*;
+import org.apache.isis.applib.query.Query;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.persistence.jdo.applib.services.JdoSupportService;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@DomainService(
+        nature = NatureOfService.VIEW,
+        logicalTypeName = "depotapp.Ajustes"
+)
+@javax.annotation.Priority(PriorityPrecedence.EARLY)
+@lombok.RequiredArgsConstructor(onConstructor_ = {@Inject} )
+public class Ajustes {
+
+    final RepositoryService repositoryService;
+    final JdoSupportService jdoSupportService;
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public Ajuste AjustePositivo(
+            Cliente cliente
+    )
+    {
+        return repositoryService.persist(Ajuste.creacion(TipoAjuste.AJP, cliente));
+    }
+
+    @Action(semantics = SemanticsOf.NON_IDEMPOTENT)
+    @ActionLayout(promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public Ajuste AjusteNegativo(
+            Cliente cliente
+    )
+    {
+        return repositoryService.persist(Ajuste.creacion(TipoAjuste.AJN, cliente));
+    }
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT)
+    public List<Ajuste> listAll(){
+        return repositoryService.allInstances(Ajuste.class);
+    }
+
+    @Action(semantics = SemanticsOf.SAFE)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT, promptStyle = PromptStyle.DIALOG_SIDEBAR)
+    public List<Ajuste> findByCodigo(
+            @CodigoCo final String codigo
+    ) {
+        return repositoryService.allMatches(
+                Query.named(Ajuste.class, Ajuste.NAMED_QUERY__FIND_BY_CODIGO_LIKE)
+                        .withParameter("codigo", codigo));
+    }
+
+
+    @Programmatic
+    public Ajuste findByCodigoExact(final String codigo) {
+        return repositoryService.firstMatch(
+                        Query.named(Ajuste.class, Ajuste.NAMED_QUERY__FIND_BY_CODIGO_EXACT)
+                                .withParameter("codigo", codigo))
+                .orElse(null);
+    }
+
+
+
+}
