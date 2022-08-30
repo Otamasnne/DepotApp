b266d107f611a28d534dbb9f02190a34ad6beef5
MartinLnn <martin.lacabanne@gmail.com>
2022-08-29 21:01:15 -0300

    Issue #9: Implementación para la visualización de los items en cada Ajuste

--- module-simple/src/main/java/domainapp/modules/simple/dom/item/itemAjuste/Ajuste_ItemAjustes.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/item/itemAjuste/Ajuste_ItemAjustes.java
@@ -1,4 +1,21 @@
 package domainapp.modules.simple.dom.item.itemAjuste;
 
+import domainapp.modules.simple.dom.ajuste.Ajuste;
+import lombok.RequiredArgsConstructor;
+import org.apache.isis.applib.annotation.Collection;
+import org.apache.isis.applib.annotation.CollectionLayout;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Collection
+@CollectionLayout(defaultView = "table")
+@RequiredArgsConstructor
 public class Ajuste_ItemAjustes {
+
+    private final Ajuste ajuste;
+
+    public List<ItemAjuste> coll() { return itemAjusteRepository.buscarItemPorAjuste(ajuste) ; }
+    
+    @Inject ItemAjusteRepository itemAjusteRepository;
 }
