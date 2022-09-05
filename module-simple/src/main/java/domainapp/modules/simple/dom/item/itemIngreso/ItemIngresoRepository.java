3c825e639ea2589af3cfde3da56575ad78b46929
MartinLnn <martin.lacabanne@gmail.com>
2022-09-05 19:35:56 -0300

    Issue #7: Implementación de la clase ItemIngresoRepository

--- module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/ItemIngresoRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/ItemIngresoRepository.java
@@ -1,4 +1,33 @@
 package domainapp.modules.simple.dom.item.itemIngreso;
 
+import domainapp.modules.simple.dom.articulo.Articulo;
+import domainapp.modules.simple.dom.ingreso.Ingreso;
+import lombok.RequiredArgsConstructor;
+import org.apache.isis.applib.services.repository.RepositoryService;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Repository
+@RequiredArgsConstructor(onConstructor_ = {@Inject} )
 public class ItemIngresoRepository {
+
+    private final RepositoryService repositoryService;
+
+    public List<ItemIngreso> buscarItemPorIngreso(Ingreso ingreso) {
+        return repositoryService.allInstances(ItemIngreso.class).stream()
+                .filter(x -> x.getIngreso().equals(ingreso))
+                .collect(Collectors.toList());
+    }
+
+    public Optional<ItemIngreso> buscarItemPorIngresoYArticulo(Ingreso ingreso, Articulo articulo) {
+        return repositoryService.allInstances(ItemIngreso.class).stream()
+                .filter(x -> x.getIngreso().equals(ingreso))
+                .filter(x -> x.getArticulo().equals(articulo))
+                .findFirst();
+    }
+
 }
