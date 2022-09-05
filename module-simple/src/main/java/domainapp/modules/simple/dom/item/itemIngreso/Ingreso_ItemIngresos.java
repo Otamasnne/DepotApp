859c31daf3c2e83b639b42cb767f03607205b9ac
MartinLnn <martin.lacabanne@gmail.com>
2022-09-05 19:38:19 -0300

    Issue #7: Implementación de clase Ingreso_ItemIngresos.
    Mixin class para devolver el listado de los items correspondientes al ingreso en el que cual estamos parados.

--- module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/Ingreso_ItemIngresos.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/Ingreso_ItemIngresos.java
@@ -1,4 +1,22 @@
 package domainapp.modules.simple.dom.item.itemIngreso;
 
+import domainapp.modules.simple.dom.ingreso.Ingreso;
+import lombok.RequiredArgsConstructor;
+import org.apache.isis.applib.annotation.Collection;
+import org.apache.isis.applib.annotation.CollectionLayout;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@Collection
+@CollectionLayout(defaultView = "table")
+@RequiredArgsConstructor
 public class Ingreso_ItemIngresos {
+
+    private final Ingreso ingreso;
+
+    public List<ItemIngreso> coll() { return itemIngresoRepository.buscarItemPorIngreso(ingreso) ; }
+
+    @Inject
+    ItemIngresoRepository itemIngresoRepository;
 }
