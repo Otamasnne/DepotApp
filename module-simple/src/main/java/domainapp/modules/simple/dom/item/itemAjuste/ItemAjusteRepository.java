466b7e043cae93bd9f4374a290650b8f1b541ab4
MartinLnn <martin.lacabanne@gmail.com>
2022-09-10 20:08:34 -0300

    Issue #9: Se pasa el repositorio a Queries, reemplazando Streams

--- module-simple/src/main/java/domainapp/modules/simple/dom/item/itemAjuste/ItemAjusteRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/item/itemAjuste/ItemAjusteRepository.java
@@ -3,13 +3,13 @@ package domainapp.modules.simple.dom.item.itemAjuste;
 import domainapp.modules.simple.dom.ajuste.Ajuste;
 import domainapp.modules.simple.dom.articulo.Articulo;
 import lombok.RequiredArgsConstructor;
+import org.apache.isis.applib.query.Query;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor(onConstructor_ = {@Inject} )
@@ -18,15 +18,15 @@ public class ItemAjusteRepository {
     private final RepositoryService repositoryService;
 
     public List<ItemAjuste> buscarItemPorAjuste(Ajuste ajuste) {
-        return repositoryService.allInstances(ItemAjuste.class).stream()
-                .filter(x -> x.getAjuste().equals(ajuste))
-                .collect(Collectors.toList());
+        return repositoryService.allMatches(
+                Query.named(ItemAjuste.class, ItemAjuste.NAMED_QUERY__BUSCAR_ITEM_POR_AJUSTE)
+                        .withParameter("ajuste", ajuste));
     }
 
     public Optional<ItemAjuste> buscarItemPorAjusteYArticulo(Ajuste ajuste, Articulo articulo) {
-        return repositoryService.allInstances(ItemAjuste.class).stream()
-                .filter(x -> x.getAjuste().equals(ajuste))
-                .filter(x -> x.getArticulo().equals(articulo))
-                .findFirst();
+        return repositoryService.firstMatch(
+                Query.named(ItemAjuste.class, ItemAjuste.NAMED_QUERY__BUSCAR_ITEM_POR_AJUSTE_Y_ARTICULO)
+                        .withParameter("ajuste", ajuste)
+                        .withParameter("articulo", articulo));
     }
 }
