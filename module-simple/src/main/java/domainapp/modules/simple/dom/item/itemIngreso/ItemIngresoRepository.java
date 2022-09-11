0a298d1a0b4c5086876cb3c7d1d71d551ac7d12a
MartinLnn <martin.lacabanne@gmail.com>
2022-09-10 23:32:39 -0300

    Issue #7: Nuevas Queries para el repositorio
    Se pasan de Streams a Queries de JDOQL

--- module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/ItemIngresoRepository.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/item/itemIngreso/ItemIngresoRepository.java
@@ -3,13 +3,13 @@ package domainapp.modules.simple.dom.item.itemIngreso;
 import domainapp.modules.simple.dom.articulo.Articulo;
 import domainapp.modules.simple.dom.ingreso.Ingreso;
 import lombok.RequiredArgsConstructor;
+import org.apache.isis.applib.query.Query;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.springframework.stereotype.Repository;
 
 import javax.inject.Inject;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor(onConstructor_ = {@Inject} )
@@ -18,16 +18,16 @@ public class ItemIngresoRepository {
     private final RepositoryService repositoryService;
 
     public List<ItemIngreso> buscarItemPorIngreso(Ingreso ingreso) {
-        return repositoryService.allInstances(ItemIngreso.class).stream()
-                .filter(x -> x.getIngreso().equals(ingreso))
-                .collect(Collectors.toList());
+        return repositoryService.allMatches(
+                Query.named(ItemIngreso.class, ItemIngreso.NAMED_QUERY_BUSCAR_ITEM_POR_INGRESO)
+                        .withParameter("ingreso", ingreso));
     }
 
     public Optional<ItemIngreso> buscarItemPorIngresoYArticulo(Ingreso ingreso, Articulo articulo) {
-        return repositoryService.allInstances(ItemIngreso.class).stream()
-                .filter(x -> x.getIngreso().equals(ingreso))
-                .filter(x -> x.getArticulo().equals(articulo))
-                .findFirst();
+        return repositoryService.firstMatch(
+                Query.named(ItemIngreso.class, ItemIngreso.NAMED_QUERY_BUSCAR_ITEM_POR_INGRESO_Y_ARTICULO)
+                        .withParameter("ingreso", ingreso)
+                        .withParameter("articulo", articulo));
     }
 
 }
