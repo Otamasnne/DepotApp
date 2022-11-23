90e41f1b0fcefa66991dc047754100ab94b347a3
nachitoota13 <ignaciootarola13@gmail.com>
2022-11-23 19:02:03 -0300

    Issue #15: Configuracion de clases Se sube la configuracion correspondiente para que los reportes funcionen

--- module-simple/src/main/java/domainapp/modules/simple/dom/reportes/Reportes.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/reportes/Reportes.java
@@ -3,6 +3,10 @@ package domainapp.modules.simple.dom.reportes;
 
 import domainapp.modules.simple.dom.articulo.Articulos;
 import domainapp.modules.simple.dom.cliente.Clientes;
+import domainapp.modules.simple.dom.encabezado.ingreso.Ingresos;
+import domainapp.modules.simple.dom.encabezado.pedido.Pedidos;
+import domainapp.modules.simple.dom.ubicacion.Ubicacion;
+import domainapp.modules.simple.dom.ubicacion.Ubicaciones;
 import net.sf.jasperreports.engine.JRException;
 import org.apache.isis.applib.services.repository.RepositoryService;
 import org.apache.isis.applib.annotation.*;
@@ -25,9 +29,34 @@ class Reportes {
         return clientes.generarReporteCliente();
     }
 
+    @Action(semantics = SemanticsOf.SAFE, restrictTo = RestrictTo.PROTOTYPING)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT, named = "Exportar Ingresos PDF", sequence = "1")
+    public Blob generarReporteIngresos() throws JRException, IOException{
+        return ingresos.generarReporteIngreso();
+    }
+
+
+    @Action(semantics = SemanticsOf.SAFE, restrictTo = RestrictTo.PROTOTYPING)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT, named = "Exportar Pedidos PDF", sequence = "1")
+    public Blob generarReportePedidos() throws JRException, IOException{
+        return pedidos.generarReportePedido();
+    }
+
+    @Action(semantics = SemanticsOf.SAFE, restrictTo = RestrictTo.PROTOTYPING)
+    @ActionLayout(bookmarking = BookmarkPolicy.AS_ROOT, named = "Exportar Ubicaciones PDF", sequence = "1")
+    public Blob generarReporteUbicaciones() throws JRException, IOException{
+        return ubicaciones.generarReporteUbicacion();
+    }
+
 
     @Inject RepositoryService repositoryService;
     @Inject Articulos articulos;
     @Inject Clientes clientes;
+    @Inject Ingresos ingresos;
+
+    @Inject Pedidos pedidos;
+
+    @Inject Ubicaciones ubicaciones;
+
 
 }
