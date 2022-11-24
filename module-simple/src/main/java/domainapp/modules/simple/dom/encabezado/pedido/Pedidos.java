5d3eaa46c0a11a7fd896c8677f718fd19be10730
nachitoota13 <ignaciootarola13@gmail.com>
2022-11-23 21:18:34 -0300

    Issue #15: Creacion de reporte PEDIDO Se crearon las clases, configuraciones y jasper correspondiente a pedidos

--- module-simple/src/main/java/domainapp/modules/simple/dom/encabezado/pedido/Pedidos.java
+++ module-simple/src/main/java/domainapp/modules/simple/dom/encabezado/pedido/Pedidos.java
@@ -1,15 +1,20 @@
 package domainapp.modules.simple.dom.encabezado.pedido;
 
+
 import domainapp.modules.simple.dom.articulo.Articulo;
 import domainapp.modules.simple.dom.cliente.Cliente;
 import domainapp.modules.simple.dom.item.itemPedido.ItemPedido;
 import lombok.RequiredArgsConstructor;
+import net.sf.jasperreports.engine.JRException;
 import org.apache.isis.applib.annotation.*;
 import org.apache.isis.applib.query.Query;
 import org.apache.isis.applib.services.repository.RepositoryService;
+import org.apache.isis.applib.value.Blob;
 import org.apache.isis.persistence.jdo.applib.services.JdoSupportService;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @DomainService(
@@ -53,5 +58,14 @@ public class Pedidos {
                         .withParameter("codigo", codigo))
                 .orElse(null);
     }
+
+    @Programmatic
+    public Blob generarReportePedido() throws JRException, IOException {
+        List<Pedido> pedidos = new ArrayList<Pedido>();
+        reportePadre ReportePadre = new reportePadre();
+        pedidos = repositoryService.allInstances(Pedido.class);
+        return ReportePadre.ListadoPedidosPDF(pedidos);
+    }
+
 
 }
